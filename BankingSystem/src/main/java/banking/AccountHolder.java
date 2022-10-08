928124271df648d0c57f1312108df0c79f814fd1
vivekmalali <88707518+vivekmalali@users.noreply.github.com>
2022-10-08 12:51:56 +0800

    Update AccountHolder.java

--- BankingSystem/src/main/java/banking/AccountHolder.java
+++ BankingSystem/src/main/java/banking/AccountHolder.java
@@ -1,20 +1,15 @@
 package banking;
 
 public abstract class AccountHolder {
+	
 	private int idNumber;
-
-	/**
-	 * @param idNumber The government-issued ID used during account setup.
-	 */
-	public AccountHolder(int idNumber){
-		// complete the function
+	
+	public AccountHolder(int idNumber) {
+		this.idNumber = idNumber;
 	}
-
-	/**
-	 * @return private int {@link AccountHolder#idNumber}
-	 */
-	public int getIdNumber() {
-		// complete the function
-        return -1;
+     
+	public int gerIdNumber() {
+		return idNumber;
 	}
 }
+
