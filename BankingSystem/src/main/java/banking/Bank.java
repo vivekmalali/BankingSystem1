7185e4d372867d3e360e3793dfd867f9dbbc5d5e
vivekmalali <88707518+vivekmalali@users.noreply.github.com>
2022-10-08 12:52:45 +0800

    Update Bank.java

--- BankingSystem/src/main/java/banking/Bank.java
+++ BankingSystem/src/main/java/banking/Bank.java
@@ -6,44 +6,56 @@ import java.util.LinkedHashMap;
  * Private Variables:<br>
  * {@link #accounts}: List&lt;Long, Account&gt;
  */
-public class Bank implements BankInterface {
-	private LinkedHashMap<Long, Account> accounts;
-
-	public Bank() {
-		// complete the function
-	}
-
-	private Account getAccount(Long accountNumber) {
-		// complete the function
-        return null;
-	}
-
-	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
-	}
-
-	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
-	}
-
-	public boolean authenticateUser(Long accountNumber, int pin) {
-		// complete the function
-        return true;
-	}
-
-	public double getBalance(Long accountNumber) {
-		// complete the function
-        return -1;
-	}
-
-	public void credit(Long accountNumber, double amount) {
-		// complete the function
-	}
-
-	public boolean debit(Long accountNumber, double amount) {
-		// complete the function
-        return true;
-	}
+public class Bank implements BankInterface{
+	
+	private LinkedHashMap<Long, Account> accounts = new LinkedHashMap<Long, Account>();
+	private static int accSeq = 0;
+	private int accNum = 0;
+    public Bank() {
+    	
+    }
+    
+    public Account getAccount(Long accountNumber) {
+    	return accounts.get(accountNumber);
+    }
+    
+    public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
+    	accNum = ++accSeq;
+    	accounts.put(new Long(accNum), new CommercialAccount(company, new Long(accNum), pin,startingDeposit));
+    	return new Long(accNum);
+    }
+    
+    public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
+    	accNum = ++accSeq;
+    	accounts.put(new Long(accNum), new ConsumerAccount(person, new Long(accNum), pin,startingDeposit));
+    	return new Long(accNum);
+    }
+    
+    public boolean authenticateUser(Long accountNumber, int pin) {
+    	if(accounts.get(accountNumber).getPin() == pin) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public double getBalance(Long accountNumber) {
+    	return accounts.get(accountNumber).getBalance();
+    }
+    
+    public void debit(Long accountNumber, double debitAmount) {
+    	
+    	double balAmt = accounts.get(accountNumber).getBalance() - debitAmount;
+    	Account obj = accounts.get(accountNumber);
+    	obj.setBalance(balAmt);
+    	accounts.put(accountNumber, obj);
+    }
+    
+  public void credit(Long accountNumber, double creditAmount) {
+    	
+    	double balAmt = accounts.get(accountNumber).getBalance() + creditAmount;
+    	Account obj = accounts.get(accountNumber);
+    	obj.setBalance(balAmt);
+    	accounts.put(accountNumber, obj);
+    }
+    
 }
